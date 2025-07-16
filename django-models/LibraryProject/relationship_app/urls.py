d7d04b505b49dc971df6d1127b6c1ccca9a64279
anas amellahe <amellaheanas@gmail.com>
2025-07-17 00:49:09 +0100

    1. Django Views and URL Configuration

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.list_book.as_view()),
+    path('<str:title>/', views.library_detail.as_view()),
+] 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import View, ListView, DeleteView
+from .models import Book, Library
+
+
+class list_book(ListView):
+    model = Book
+    template_name = 'relationship_app/list_books.html'
+    context_object_name = 'books'
+
+class library_detail(DeleteView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+    slug_field = 'name'
+    slug_url_kwarg = 'title'
 
 # Create your views here.
