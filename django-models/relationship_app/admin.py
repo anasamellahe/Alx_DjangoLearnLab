a27fcdd07589f3cc6f2291050be7d63a82f88d08
anas amellahe <amellaheanas@gmail.com>
2025-07-16 18:24:07 +0100

    0. Implementing Advanced Model Relationships in Django

---
+++ django-models/relationship_app/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+from .models import Library, Librarian, Book, Author
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'get_Author_name')
+    list_filter = ('author__name',)
+    search_fields = ('title', 'author__name')
+    def get_Author_name(self, obj):
+        return obj.author.name if obj.author else 'No Library'
+    get_Author_name.short_description = 'Author'
+
+
+class LibraryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'get_books_count')
+    filter_horizontal = ('books',)
+    
+    def get_books_count(self, obj):
+        return obj.books.count()
+    get_books_count.short_description = 'Number of Books'
+
+
+class LibrarianAdmin(admin.ModelAdmin):
+    list_display = ('name', 'get_library_name')
+    
+    def get_library_name(self, obj):
+        return obj.Library.name if obj.Library else 'No Library'
+    get_library_name.short_description = 'Library'
+
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register(Library, LibraryAdmin)
+admin.site.register(Librarian, LibrarianAdmin)
---
+++ django-models/relationship_app/query_samples.py
@@ -0,0 +1,26 @@
+# This script should contain the query for each of the following of relationship:
+# Query all books by a specific author.
+# List all books in a library.
+# Retrieve the librarian for a library.
+
+from .models import Book, Author, Librarian, Library
+
+# Query all books by a specific author.
+print("=== Query all books by a specific author ===")
+books_by_author = Book.objects.filter(author__name='test')
+for book in books_by_author:
+    print(f"Book: {book.title}")
+
+print("\n=== List all books in a library ===")
+# List all books in a library.
+library_instance = Library.objects.get(name='anas')
+books_in_library = library_instance.books.all()
+
+for book in books_in_library:
+    print(f"Book in library: {book.title}")
+
+print("\n=== Retrieve the librarian for a library ===")
+# Retrieve the librarian for a library.
+library_for_librarian = Library.objects.get(name='casa')
+librarian_for_library = library_for_librarian.library  # Using related_name to get the librarian object
+print(f"Librarian for {library_for_librarian.name}: {librarian_for_library.name}")
